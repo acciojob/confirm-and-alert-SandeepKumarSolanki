bd0a2b9c8363f8d614b2be4af0de707383d68653
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-05 23:43:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let flag = false;
+
+function confirmation(){
+	flag = true;
+	if(flag){
+		alert(`You clicked OK. Proceeding...`)
+	}else{
+		setTimeout(()=>{
+			alert(`You clicked Cancel. Exiting...`)
+		},2000)
+	}
+}
+
+document.querySelector('#btn').addEventListener("click" , confirmation)
+
